0cd03ac5b1c22616e5b910d47729c7763cd727b2
Jayapriya Surendran <priya7390@gmail.com>
2016-03-27 00:39:30 -0500

    initial endpoints implementation

---
+++ thrift/fileserver.thrift
@@ -0,0 +1,15 @@
+include "shared.thrift"
+
+namespace java edu.umn.cs.distributedkeyvaluestore
+
+service FileServerEndPoints {
+     // called by clients
+     shared.ReadResponse read(1:string filename);
+     shared.WriteResponse write(1:string filename, 2:string contents);
+     shared.FileServerMetaData getFileServerMetadata();
+
+     // called by coordinator
+     shared.ReadResponse readContents(1:string filename);
+     shared.WriteResponse writeContents(1:string filename, 2:string contents, 3:i64 version);
+     i64 getVersion(1:string filename);
+}
